b762a335e80f5c898815af2710e91a0b366ffec3
rmslick <rmslick@buffalo.edu>
2021-12-05 14:34:23 -0500

    Cleaned repo

--- GaborFilter.py
+++
@@ -1,7 +0,0 @@
-import SpykeTorch.utils as utils
-
-kernels = [	utils.GaborKernel(window_size = 3, orientation = 45+22.5),
-            utils.GaborKernel(3, 90+22.5),
-            utils.GaborKernel(3, 135+22.5),
-            utils.GaborKernel(3, 180+22.5)]
-filter = utils.Filter(kernels, use_abs = True)
